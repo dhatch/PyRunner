92ebe44087255f42622352e46997cd5210fd265e
Russ <russplaysguitar@yahoo.com>
2011-07-02 23:35:38 -0700

    Added horizontal blocks, made block size random

--- pyRunner.py
+++ pyRunner.py
@@ -336,7 +336,10 @@ class fadeEffect(pygame.sprite.Sprite):
 class block(scroller):
     def __init__(self, y):
         scroller.__init__(self)
-        self.image = pygame.Surface((20, 90))
+        self.side1 = random.randint(20,100)
+        self.side2 = 20
+        self.dimensions = random.choice([(self.side1,self.side2),(self.side2,self.side1)])
+        self.image = pygame.Surface(self.dimensions)
         self.rect = self.image.get_rect()
         self.y = y
         pygame.draw.rect(self.image, (28,55,183), self.rect)
