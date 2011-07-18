646b462297a57d0dfafe07c537b3c6263a5f1eeb
David Hatch <dhatch387@gmail.com>
2011-07-18 09:59:43 -0400

    Revert "Revert "Removed OSX-only temp fix, now that memory leak is fixed""
    This reverts commit 5208523f3553a41b07550957a4b13b7ea35cc4e4.
    
    Conflicts:
    
    	pyRunner.py

--- pyRunner.py
+++ pyRunner.py
@@ -776,7 +776,7 @@ def gameInit():
     shieldGroup = randomRezGroup(shieldCube,maxRezHeight=blockMaxHeight,minRezHeight=blockMinHeight)
     turretGroup = randomRezGroup(turret,maxRezHeight = blockMaxHeight,minRezHeight=blockMinHeight)
     gunGroup = randomRezGroup(gunCube,maxRezHeight = blockMaxHeight,minRezHeight = blockMinHeight)
-    effectsGroup = pygame.sprite.Group()
+    effectsGroup = WorkingSingle() #group to store effects in
     bulletGroup = pygame.sprite.RenderUpdates()
     gunnerGroup = pygame.sprite.RenderUpdates()
     ##LEVEL CREATION AND DESIGN
